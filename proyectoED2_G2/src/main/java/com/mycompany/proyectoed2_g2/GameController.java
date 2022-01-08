b0c686a434bff0cc7da58380799eac0eeac8e1c6
anvefran <anvefran@espol.edu.ec>
2022-01-08 12:27:06 -0500

    Se modifica el formato y los colores, tambien se agregan las alertas para empate y ganador. Finalizacion del juego

--- proyectoED2_G2/src/main/java/com/mycompany/proyectoed2_g2/GameController.java
+++ proyectoED2_G2/src/main/java/com/mycompany/proyectoed2_g2/GameController.java
@@ -1,12 +1,18 @@
 package com.mycompany.proyectoed2_g2;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import modelo.Game;
 import modelo.Tablero;
 
@@ -16,6 +22,11 @@ public class GameController {
     private GridPane gamePane;
     
     private Tablero tableGame;
+    
+    private Character playerSymbol = MainMenuController.playerSymbol;
+    private Character aiSymbol = MainMenuController.aiSymbol;
+    private Character firstSymbol = MainMenuController.firstSymbol;
+    
 
     @FXML
     void switchToMainMenu(ActionEvent event) {
@@ -31,6 +42,11 @@ public class GameController {
     private void initGame(){
         tableGame = new Tablero();
         updateUITable();
+        
+        /*Verifica si la maquina debe empezar primero*/
+        if(!firstSymbol.equals(playerSymbol)){
+            aiMove();
+        }
     }
 
     private void updateUITable() {
@@ -42,43 +58,115 @@ public class GameController {
                 Character currentChar = matrix[y][x];
                 
                 VBox vbox = new VBox();
-                vbox.getChildren().add(new Text(Character.toString(currentChar)));
                 vbox.setPrefSize(100, 100);
-                vbox.setAlignment(Pos.CENTER);        
+                vbox.setAlignment(Pos.CENTER);  
                 
+                
+                loadSymbolImage(currentChar,vbox);    /*Carga la imagen del simbolo*/
+
                 final int indexX = x;
                 final int indexY = y;
                 
                 
                 vbox.setOnMouseClicked(e -> {
-                    tableGame.getMatrix()[indexY][indexX] = 'X';
-                    updateUITable();
-                    aiMove();
-                    updateUITable();
-                    tableGame.print();
                     
-                    if(!Game.continuarJuego(tableGame)){
-                        mostrarAlerta(Alert.AlertType.INFORMATION, "Alguien ha ganado");
+                    if(tableGame.getMatrix()[indexY][indexX] == ' '){
+                        tableGame.getMatrix()[indexY][indexX] =  playerSymbol;
+                        updateUITable();  /*Actualiza el GridPane*/
+                        //verifyWinner();   /*Verifica si alguien ha ganado*/
+                        aiMove();         /*Realiza el movimiento de la maquina*/
+                        verifyWinner();   /*Verifica si alguien ha ganado*/
+                        
+                        
+                    } else {
+                        /*La posicion no debe estar ocupada por otra ficha*/
+                        
+                        mostrarAlerta(Alert.AlertType.ERROR, "No puede colocar en una posicion ya ocupada");
+                        System.out.println("Ya se encuentra ocupado");
                     }
                     
                 });
-                
                 gamePane.add(vbox, x, y);
             }
         }
     }
     
     private void aiMove(){
-        Game.generarMove(tableGame, 'O');
-        //updateUITable();
+        Game.generarMove(tableGame, aiSymbol);
+        updateUITable();
     }
     
     private void mostrarAlerta(Alert.AlertType tipo, String mensaje) {
         Alert alert = new Alert(tipo);
 
-        alert.setTitle("Resultado de operacion");
+        alert.setTitle("Informacion");
         alert.setHeaderText("Notificacion");
         alert.setContentText(mensaje);
         alert.showAndWait();
+    }
+    
+    private void loadSymbolImage(Character currentChar, VBox vbox) {
+        if (currentChar == 'X') {
+            Image img = new Image("images/x.png", 90, 90, false, false);
+            ImageView imv = new ImageView(img);
+            vbox.getChildren().add(imv);
+        }
+
+        if (currentChar == 'O') {
+            Image img = new Image("images/o.png", 100, 100, false, false);
+            ImageView imv = new ImageView(img);
+            vbox.getChildren().add(imv);
+
+        }
+    }
+    
+    private int verifyWinner(){
+        if(!Game.continuarJuego(tableGame)){
+            mostrarAlerta(Alert.AlertType.INFORMATION, "Alguien ha ganado");
+            volverMenu();
+            return 0;
+        }
+        
+        if(isTie()){
+            mostrarAlerta(Alert.AlertType.INFORMATION, "Han empatado!");
+            volverMenu();
+            return 0;
+        }
+        
+        return 1;
+
+    }
+    
+    private void volverMenu() {
+    	try {
+            /*Cierra la ventana actual*/
+            Stage gameStage = (Stage) gamePane.getScene().getWindow();
+            gameStage.close();
+            
+            /*Carga la pantalla principal*/
+            Parent root = App.loadFXML("MainMenu");
+            Stage stage  = new Stage();
+            Scene scene = new Scene(root, 600, 400);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.show();
+            
+        } catch (IOException e){
+            System.out.println("File not found, Error al cargar pantalla");
+        }
+    }
+    
+    
+    public boolean isTie(){
+        
+        Character[][] matrix = tableGame.getMatrix();
+        for(int i = 0; i<matrix.length ; i++ ){
+            for(int j = 0; j<matrix[i].length ; j++){
+                if(matrix[i][j] == ' '){
+                    return false;
+                }
+            }
+        }
+        return true && Game.continuarJuego(tableGame);
     }
 }
--- proyectoED2_G2/src/main/java/com/mycompany/proyectoed2_g2/MainMenuController.java
+++ proyectoED2_G2/src/main/java/com/mycompany/proyectoed2_g2/MainMenuController.java
@@ -4,30 +4,64 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 
 public class MainMenuController {
     
     @FXML
-    private ComboBox<String> playerSymbol;
+    private ComboBox<Character> playerSymbolBox;
     
     @FXML
-    private ComboBox<String> playerFirst;
+    private ComboBox<Character> playerFirstBox;
+    
+    //info
+    public static Character playerSymbol = ' ';
+    public static Character aiSymbol = ' ';
+    public static Character firstSymbol = ' ';
     
     @FXML
     private void initialize() {
    	
-   	ObservableList<String> items = FXCollections.observableArrayList("X", "O");
-   	playerSymbol.setItems(items);
-        playerFirst.setItems(items);
+   	ObservableList<Character> items = FXCollections.observableArrayList('X', 'O');
+   	playerSymbolBox.setItems(items);
+        playerFirstBox.setItems(items);
         
     }
     
     @FXML
     void switchToGame(ActionEvent event) {
-        App.switchScenes(event, "Game", 800, 600);
+        /*Verificacion si los campos son nulos*/
+        if(playerSymbolBox.getValue()!=null && playerFirstBox.getValue() != null){
+            playerSymbol = playerSymbolBox.getValue();
+            setOppositeSymbol(playerSymbol);
+            firstSymbol = playerFirstBox.getValue();
+            
+            //se hace el cambio de escena
+            App.switchScenes(event, "Game", 800, 600);
+        } else {
+            mostrarAlerta(Alert.AlertType.ERROR, "Complete todos los campos para iniciar partida");
+            //System.out.println("Complete todos los campos para iniciar partida");
+        }
+        
+        
     }
     
+    public void setOppositeSymbol(Character symbol){
+        if(symbol.equals('X')){
+            aiSymbol = 'O';
+        } else {
+            aiSymbol = 'X';
+        }
+    }
     
+    private void mostrarAlerta(Alert.AlertType tipo, String mensaje) {
+        Alert alert = new Alert(tipo);
+
+        alert.setTitle("Informacion");
+        alert.setHeaderText("Notificacion");
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
 
 }
